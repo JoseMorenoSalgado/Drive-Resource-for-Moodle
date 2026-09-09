c2f34b751c90cf7c45e18ed2ef961821ca0016ae
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-09 10:07:25 -0600

    test: fix Moodle 4.5 coverage metadata

--- tests/drive_test.php
+++ tests/drive_test.php
@@ -26,7 +26,7 @@ use PHPUnit\Framework\Attributes\DataProvider;
  * @package    mod_videoplayer
  * @category   test
  * @copyright  2026 Jose Erasmo Moreno Salgado - Elearning Cloud
- * @covers     \\mod_videoplayer\\local\\drive
+ * @covers     \mod_videoplayer\local\drive
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 #[CoversClass(drive::class)]
