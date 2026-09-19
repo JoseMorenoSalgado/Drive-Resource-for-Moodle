8ebeb313b9e5d2810b533a36ecb8ccb913f8fe11
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-19 17:11:23 -0600

    release: bump production candidate to 1.1.32-rc2

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026091900;
-$plugin->release = '1.1.32-rc1';
+$plugin->version = 2026091901;
+$plugin->release = '1.1.32-rc2';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502];
 $plugin->maturity = MATURITY_RC;
