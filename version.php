a6d7cbfd86bbbeb79f69db3a60bec008dd9d39a9
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-19 16:20:23 -0600

    Prepare Drive Resource 1.1.32 RC1

--- version.php
+++ version.php
@@ -26,8 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026090901;
-$plugin->release = '1.1.31-beta';
+$plugin->version = 2026091900;
+$plugin->release = '1.1.32-rc1';
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502];
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_RC;
