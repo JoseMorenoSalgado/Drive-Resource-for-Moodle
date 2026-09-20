486592d09072ead9861b0797dc07a45746dada69
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-19 21:59:02 -0600

    release: add rc9 cache invalidation savepoint

--- db/upgrade.php
+++ db/upgrade.php
@@ -738,5 +738,11 @@ function xmldb_videoplayer_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2026092004, 'videoplayer');
     }
 
+    if ($oldversion < 2026092005) {
+        // No schema change is required. This savepoint invalidates caches for
+        // the RC9 protected video health and recovery UI.
+        upgrade_mod_savepoint(true, 2026092005, 'videoplayer');
+    }
+
     return true;
 }
