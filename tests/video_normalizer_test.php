063975a2f9d5f2a0f1685379fbd2e65fd8c0145b
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-19 23:49:43 -0600

    test(video): cover container and 10-bit H264 normalization

--- tests/video_normalizer_test.php
+++ tests/video_normalizer_test.php
@@ -27,6 +27,7 @@ final class video_normalizer_test extends \advanced_testcase {
      */
     public function test_h264_aac_is_web_compatible(): void {
         $probe = [
+            'format' => ['format_name' => 'mov,mp4,m4a,3gp,3g2,mj2'],
             'streams' => [
                 [
                     'codec_type' => 'video',
@@ -50,6 +51,7 @@ final class video_normalizer_test extends \advanced_testcase {
      */
     public function test_tscc2_requires_normalization(): void {
         $probe = [
+            'format' => ['format_name' => 'mov,mp4,m4a,3gp,3g2,mj2'],
             'streams' => [
                 [
                     'codec_type' => 'video',
@@ -73,19 +75,38 @@ final class video_normalizer_test extends \advanced_testcase {
      */
     public function test_nonportable_h264_variants_require_normalization(): void {
         $mp3audio = [
+            'format' => ['format_name' => 'mov,mp4,m4a,3gp,3g2,mj2'],
             'streams' => [
                 ['codec_type' => 'video', 'codec_name' => 'h264', 'pix_fmt' => 'yuv420p'],
                 ['codec_type' => 'audio', 'codec_name' => 'mp3'],
             ],
         ];
         $yuv444 = [
+            'format' => ['format_name' => 'mov,mp4,m4a,3gp,3g2,mj2'],
             'streams' => [
                 ['codec_type' => 'video', 'codec_name' => 'h264', 'pix_fmt' => 'yuv444p'],
                 ['codec_type' => 'audio', 'codec_name' => 'aac'],
             ],
         ];
 
+        $h264tenbit = [
+            'format' => ['format_name' => 'mov,mp4,m4a,3gp,3g2,mj2'],
+            'streams' => [
+                ['codec_type' => 'video', 'codec_name' => 'h264', 'pix_fmt' => 'yuv420p10le'],
+                ['codec_type' => 'audio', 'codec_name' => 'aac'],
+            ],
+        ];
+        $matroska = [
+            'format' => ['format_name' => 'matroska,webm'],
+            'streams' => [
+                ['codec_type' => 'video', 'codec_name' => 'h264', 'pix_fmt' => 'yuv420p'],
+                ['codec_type' => 'audio', 'codec_name' => 'aac'],
+            ],
+        ];
+
         $this->assertFalse(video_normalizer::probe_is_web_compatible($mp3audio));
         $this->assertFalse(video_normalizer::probe_is_web_compatible($yuv444));
+        $this->assertFalse(video_normalizer::probe_is_web_compatible($h264tenbit));
+        $this->assertFalse(video_normalizer::probe_is_web_compatible($matroska));
     }
 }
