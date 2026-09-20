281967f4f28498a4780a47e70ae56f1576b06360
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-20 16:31:39 -0600

    backup: include progress completion rule state

--- backup/moodle2/backup_videoplayer_stepslib.php
+++ backup/moodle2/backup_videoplayer_stepslib.php
@@ -58,6 +58,7 @@ class backup_videoplayer_activity_structure_step extends backup_activity_structu
             'starttime',
             'endtime',
             'completionpercentage',
+            'completionprogressenabled',
             'grade',
             'displayoptions',
             'posterimage',
