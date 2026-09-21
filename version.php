f44ca3b6daa2109cf1e341bf4f3c24851199682c
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-21 13:19:42 -0600

    Start Drive Resource 1.2 Bunny beta

--- version.php
+++ version.php
@@ -27,9 +27,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026092016;
-$plugin->release = '1.1.33-rc19-m45';
+$plugin->version = 2026092103;
+$plugin->release = '1.2.0-beta1-m45';
 $plugin->requires = 2024100700; // Moodle 4.5.0 (LTS).
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_BETA;
