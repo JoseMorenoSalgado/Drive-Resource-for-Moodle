845205607cfcfe6d6c4d402a327e3e040e8f99cb
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-21 21:11:13 -0600

    Fix WHMCS addon module autoload namespace

--- integrations/whmcs/modules/addons/driveresource_gateway/hooks.php
+++ integrations/whmcs/modules/addons/driveresource_gateway/hooks.php
@@ -4,7 +4,7 @@ if (!defined('WHMCS')) {
     die('This file cannot be accessed directly');
 }
 
-use WHMCS\Module\Addon\Driveresource_gateway\GatewayMaintenance;
+use WHMCS\Module\Addon\DriveresourceGateway\GatewayMaintenance;
 
 add_hook('DailyCronJob', 1, static function (): void {
     try {
