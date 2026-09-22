acd6853d7d4565163413c5bd9017c61f332c1fe5
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-21 21:06:41 -0600

    Add WHMCS gateway API endpoints

---
+++ integrations/whmcs/modules/addons/driveresource_gateway/api/asset-bind.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+driveresource_gateway_endpoint(static function ($gateway, array $context): array {
+    return $gateway->bindAsset($context['service'], $context['siteurl'], $context['payload']);
+});
