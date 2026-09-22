33da32957651e51d7b8fe581393cc08f76e3e87b
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-21 21:08:18 -0600

    Add Bunny storage reconciliation and retention cron

---
+++ integrations/whmcs/modules/addons/driveresource_gateway/hooks.php
@@ -0,0 +1,22 @@
+<?php
+
+if (!defined('WHMCS')) {
+    die('This file cannot be accessed directly');
+}
+
+use WHMCS\Module\Addon\Driveresource_gateway\GatewayMaintenance;
+
+add_hook('DailyCronJob', 1, static function (): void {
+    try {
+        (new GatewayMaintenance())->run();
+    } catch (Throwable $exception) {
+        logModuleCall(
+            'driveresource_gateway',
+            'DailyCronJob',
+            [],
+            ['exception' => get_class($exception), 'message' => $exception->getMessage()],
+            null,
+            []
+        );
+    }
+});
