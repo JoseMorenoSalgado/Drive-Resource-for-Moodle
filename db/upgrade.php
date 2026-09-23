4e3ae56c5bbc4db085f0306f19ea6ed8b52d1a85
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-22 19:21:53 -0600

    Integrate seek-safe watched-range schema upgrade

--- db/upgrade.php
+++ db/upgrade.php
@@ -451,5 +451,27 @@ function xmldb_videoplayer_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2026092103, 'videoplayer');
     }
 
+    // Persist the union of media ranges actually reproduced by the learner.
+    if ($oldversion < 2026092201) {
+        $viewstable = new xmldb_table('videoplayer_views');
+        if ($dbman->table_exists($viewstable)) {
+            $watchedranges = new xmldb_field(
+                'watchedranges',
+                XMLDB_TYPE_TEXT,
+                null,
+                null,
+                null,
+                null,
+                null,
+                'duration'
+            );
+            if (!$dbman->field_exists($viewstable, $watchedranges)) {
+                $dbman->add_field($viewstable, $watchedranges);
+            }
+        }
+
+        upgrade_mod_savepoint(true, 2026092201, 'videoplayer');
+    }
+
     return true;
 }
