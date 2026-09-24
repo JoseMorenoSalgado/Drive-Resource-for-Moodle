fd4df2046446fbd2a15029b2ae09b5af63efb7fe
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-23 20:56:37 -0600

    Bump WHMCS companion to 0.4.1

--- integrations/whmcs/modules/addons/driveresource_gateway/driveresource_gateway.php
+++ integrations/whmcs/modules/addons/driveresource_gateway/driveresource_gateway.php
@@ -26,7 +26,7 @@ function driveresource_gateway_config(): array
     return [
         'name' => 'Drive Resource Media Gateway',
         'description' => 'WHMCS authorization, quota and Elearning Stream credential boundary for Drive Resource.',
-        'version' => '0.4.0',
+        'version' => '0.4.1',
         'author' => 'Elearning Cloud',
         'fields' => [
             'bunny_library_id' => [
