88ee9c68f07b617713bd9a0caff4b1ec833bb379
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-25 08:53:25 -0600

    Release Moodle 1.2.0 RC2 schema hotfix

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026092401;
-$plugin->release = '1.2.0-rc1-m45';
+$plugin->version = 2026092501;
+$plugin->release = '1.2.0-rc2-m45';
 $plugin->requires = 2024100700; // Moodle 4.5.0 (LTS).
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
