3dbffa3e4c198dd44607a3ea7e61958adf1c8489
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-26 00:15:44 -0600

    Release Moodle 1.2.0 RC9 legacy-reference repair

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026092507;
-$plugin->release = '1.2.0-rc8-m45';
+$plugin->version = 2026092508;
+$plugin->release = '1.2.0-rc9-m45';
 $plugin->requires = 2024100700; // Moodle 4.5.0 (LTS).
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
