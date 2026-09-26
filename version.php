743df5e92e804dcf61a785785c009588943d934e
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-25 21:05:48 -0600

    Release Moodle 1.2.0 RC4 video player initialization hotfix

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videoplayer';
-$plugin->version = 2026092502;
-$plugin->release = '1.2.0-rc3-m45';
+$plugin->version = 2026092503;
+$plugin->release = '1.2.0-rc4-m45';
 $plugin->requires = 2024100700; // Moodle 4.5.0 (LTS).
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
