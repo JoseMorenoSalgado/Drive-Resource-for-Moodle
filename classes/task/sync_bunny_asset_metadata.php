61293739f859b8c79cab16af63dd03ddfc8adf0a
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-09-25 23:48:41 -0600

    Add retry task for provider metadata synchronization

---
+++ classes/task/sync_bunny_asset_metadata.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+namespace mod_videoplayer\task;
+
+use mod_videoplayer\local\provider\bunny_stream;
+use mod_videoplayer\local\whmcs_gateway_client;
+
+/**
+ * Retry provider metadata synchronisation for one Moodle activity.
+ *
+ * @package    mod_videoplayer
+ * @copyright  2026 Jose Erasmo Moreno Salgado - Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+final class sync_bunny_asset_metadata extends \core\task\adhoc_task {
+    /**
+     * Execute the metadata synchronisation.
+     */
+    public function execute(): void {
+        global $DB;
+
+        $data = $this->get_custom_data();
+        $instanceid = (int)($data->instanceid ?? 0);
+        $videoid = strtolower(trim((string)($data->videoid ?? '')));
+        if ($instanceid <= 0 || !bunny_stream::is_valid_asset_id($videoid)) {
+            return;
+        }
+
+        $instance = $DB->get_record(
+            'videoplayer',
+            ['id' => $instanceid],
+            'id, source, name, providerassetid',
+            IGNORE_MISSING
+        );
+        if (
+            !$instance
+            || ($instance->source ?? '') !== bunny_stream::SOURCE
+            || !hash_equals(
+                strtolower((string)($instance->providerassetid ?? '')),
+                $videoid
+            )
+            || trim((string)$instance->name) === ''
+        ) {
+            return;
+        }
+
+        (new whmcs_gateway_client())->update_asset_title(
+            $videoid,
+            $instanceid,
+            (string)$instance->name
+        );
+    }
+}
